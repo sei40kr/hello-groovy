808575297fbedf6b06a417b9ecfe80c44240076a
Seong Yong-ju <sei40kr@gmail.com>
2020-04-28 10:41:43 +0900

    doc: add detailed explanation for trampoline

--- closures/functional-programming/trampoline.groovy
+++ closures/functional-programming/trampoline.groovy
@@ -1,4 +1,12 @@
 def factorial
+// Closures are wrapped in a TrampolineClosure. Upon calling, a trampolined
+// Closure will call the original Closure waiting for its result. If the outcome
+// of the call is another instance of a TrampolineClosure, created perhaps as a
+// result to a call to the trampoline() method, the Closure will again be
+// invoked. This repetitive invocation of returned trampolined Closures instances
+// will continue until a value other than a trampolined Closure is returned.
+// That value will become the final result of the trampoline. That way, calls are
+// made serially, rather than filling the stack.
 factorial = { int n, def accu = 1G ->
     if (n < 2) return accu
     factorial.trampoline(n - 1, n * accu)
